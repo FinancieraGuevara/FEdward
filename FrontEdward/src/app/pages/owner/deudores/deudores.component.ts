d175f210139d364009ff1ebae2a5b530be3d54b3
Dylan Acevedo Villena <dylan.acevedo2843@gmail.com>
2024-12-02 12:03:42 -0500

    chore:Estructura

---
+++ FrontEdward/src/app/pages/owner/deudores/deudores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deudores',
+  imports: [],
+  templateUrl: './deudores.component.html',
+  styleUrl: './deudores.component.scss'
+})
+export class DeudoresComponent {
+
+}
---
+++ FrontEdward/src/app/shared/models/Solicitante/deudor.ts
@@ -0,0 +1,7 @@
+export interface DeudoresDTO {
+    id: number;
+    numero: string;
+    nombre_completo: string;
+    tipo: string;
+    deduda: number;
+  }
